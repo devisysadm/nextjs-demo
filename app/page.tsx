44ac05dab04ecfad78fd6ad68b87dcbdb25629ab
Devi Singh <Devi.Singh@tmotions.com>
2024-07-24 14:46:15 +0530

    update testing

--- app/page.tsx
+++ app/page.tsx
@@ -53,7 +53,7 @@ export default function Home() {
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find Hello yyyyyyyyyyyyyyyyyyyyyyyyyyyy in-depth information about Next.js features and API.
+            Find Hello yyyyyyyyyyyyyy hello done my side...!!!!!!!!!!!!!!11 in-depth information about Next.js features and API.
           </p>
         </a>
 
